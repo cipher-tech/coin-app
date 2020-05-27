0bafaecb529d3ab1a0f17ac88c95866082778cf9
cipher-tech <nickchibuikem@gmail.com>
2020-05-27 09:01:20 +0100

    Updates

---
+++ precache-manifest.61c31182ff7748697529c149cad98ef9.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66a09b1948e5feac7c21708ca84c5822",
+    "url": "/coin-app/index.html"
+  },
+  {
+    "revision": "0680eaf6240344da4ae6",
+    "url": "/coin-app/static/css/2.029307b4.chunk.css"
+  },
+  {
+    "revision": "8d3dcacc0f76f9f6e877",
+    "url": "/coin-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0680eaf6240344da4ae6",
+    "url": "/coin-app/static/js/2.170eb093.chunk.js"
+  },
+  {
+    "revision": "020c7853cef1e7d2357bd2a7fcb72487",
+    "url": "/coin-app/static/js/2.170eb093.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d3dcacc0f76f9f6e877",
+    "url": "/coin-app/static/js/main.4c036ffa.chunk.js"
+  },
+  {
+    "revision": "65df7410830b6007471b",
+    "url": "/coin-app/static/js/runtime-main.4e1fc714.js"
+  },
+  {
+    "revision": "d08f94fc881cbb5dd3356f662d5b61e6",
+    "url": "/coin-app/static/media/amazon-card.d08f94fc.png"
+  },
+  {
+    "revision": "37ea7c647fd0a5b0898d78840c36c45f",
+    "url": "/coin-app/static/media/amazon-cardCrop.37ea7c64.png"
+  },
+  {
+    "revision": "9acd921d83e16158b160e5e7405c73b5",
+    "url": "/coin-app/static/media/avatar1.9acd921d.jpg"
+  },
+  {
+    "revision": "9f6844b481efb80bb0d7e92e6ba9c8c6",
+    "url": "/coin-app/static/media/barChart.9f6844b4.svg"
+  },
+  {
+    "revision": "18c79a9eb269ed701cbfbe1a9454cbd1",
+    "url": "/coin-app/static/media/bill.18c79a9e.svg"
+  },
+  {
+    "revision": "9add25d0faf814cbfcb61e72f95cdb1c",
+    "url": "/coin-app/static/media/bills.9add25d0.svg"
+  },
+  {
+    "revision": "2c461d8e6a39ad80b30efec4d56c4f8b",
+    "url": "/coin-app/static/media/bitcoinSvg-2.2c461d8e.svg"
+  },
+  {
+    "revision": "e8c05a5f73d1ccffceedb11dab66ffbd",
+    "url": "/coin-app/static/media/bitcoinSvg-3.e8c05a5f.svg"
+  },
+  {
+    "revision": "12c928c68f6c5ced31ac3472d6c13411",
+    "url": "/coin-app/static/media/bitcoinSvg.12c928c6.svg"
+  },
+  {
+    "revision": "9139c31639d3618862f0af80938e9c41",
+    "url": "/coin-app/static/media/bitcoinsvg-1.9139c316.svg"
+  },
+  {
+    "revision": "80089d478ee5123297263c3f8bf4e82d",
+    "url": "/coin-app/static/media/box.80089d47.svg"
+  },
+  {
+    "revision": "60b16a4249ca1dca7367e40b13f3748c",
+    "url": "/coin-app/static/media/brandLogo.60b16a42.jpg"
+  },
+  {
+    "revision": "78250cd362c25703c4edbf3535f86fc7",
+    "url": "/coin-app/static/media/chartBars.78250cd3.svg"
+  },
+  {
+    "revision": "c9d6e59b4417f33fe6e2941b8607acf0",
+    "url": "/coin-app/static/media/cimage.c9d6e59b.png"
+  },
+  {
+    "revision": "f5fca313260754d035592bf76e48a753",
+    "url": "/coin-app/static/media/coinRate.f5fca313.svg"
+  },
+  {
+    "revision": "fce15f5b75bff351df2ca67d92208a6f",
+    "url": "/coin-app/static/media/coins.fce15f5b.svg"
+  },
+  {
+    "revision": "3e3165aadb52af8454376e424beb84d3",
+    "url": "/coin-app/static/media/dollarSymbol.3e3165aa.svg"
+  },
+  {
+    "revision": "f7817aad6ee2c28c447a529870e015c8",
+    "url": "/coin-app/static/media/dots.f7817aad.svg"
+  },
+  {
+    "revision": "b68171a29764937f0c935ef881342511",
+    "url": "/coin-app/static/media/envelope.b68171a2.svg"
+  },
+  {
+    "revision": "f836ebf554a2f5634c395367fac802b6",
+    "url": "/coin-app/static/media/ethereumDesire.f836ebf5.svg"
+  },
+  {
+    "revision": "5d8a0dc22077ddb693f8968726e0ab84",
+    "url": "/coin-app/static/media/footerImg.5d8a0dc2.jpg"
+  },
+  {
+    "revision": "ef1eeea22aeedf9a2c73a842597181e6",
+    "url": "/coin-app/static/media/home.ef1eeea2.svg"
+  },
+  {
+    "revision": "fc6ab8c1db85407399c9daf48432728b",
+    "url": "/coin-app/static/media/invest.fc6ab8c1.svg"
+  },
+  {
+    "revision": "38e23f331a30250111e2824becde6489",
+    "url": "/coin-app/static/media/invoice.38e23f33.svg"
+  },
+  {
+    "revision": "55a9341c51fbdd4b521d258ec6fe4427",
+    "url": "/coin-app/static/media/istore.55a9341c.svg"
+  },
+  {
+    "revision": "2a3dff74edd849d2c14d87e1dfd468e0",
+    "url": "/coin-app/static/media/lock.2a3dff74.svg"
+  },
+  {
+    "revision": "274b2e9483d815d6b520afbf1fc484b1",
+    "url": "/coin-app/static/media/pic5.274b2e94.jpg"
+  },
+  {
+    "revision": "c233c5fec6c43bd5ccd8a2cc065e2807",
+    "url": "/coin-app/static/media/playstore.c233c5fe.svg"
+  },
+  {
+    "revision": "53ea3fe8a5457d6d50cd740b41f65c5b",
+    "url": "/coin-app/static/media/quickDetails.53ea3fe8.png"
+  },
+  {
+    "revision": "46ec8e871e44c9e96281a41f394ef888",
+    "url": "/coin-app/static/media/rate.46ec8e87.png"
+  },
+  {
+    "revision": "296bdacb0424b5c7e5c06e7f86466d73",
+    "url": "/coin-app/static/media/review.296bdacb.svg"
+  },
+  {
+    "revision": "b9864ed443ba9869ad0a74a35942c9e3",
+    "url": "/coin-app/static/media/settings.b9864ed4.svg"
+  },
+  {
+    "revision": "de75ae84fc51f9e334407eb3d1deeef6",
+    "url": "/coin-app/static/media/transcation.de75ae84.png"
+  },
+  {
+    "revision": "8cc48fb14e0f589e48341990412aa096",
+    "url": "/coin-app/static/media/us-amazon.8cc48fb1.svg"
+  },
+  {
+    "revision": "31377b7f7d50d9d5cc00353572c2f2d1",
+    "url": "/coin-app/static/media/us-bitcoin.31377b7f.svg"
+  },
+  {
+    "revision": "fb40985e6f0918ef157f591155d5f349",
+    "url": "/coin-app/static/media/us-itunes.fb40985e.svg"
+  },
+  {
+    "revision": "df561e52e5d444a606412a1b5842778a",
+    "url": "/coin-app/static/media/us-pm.df561e52.svg"
+  },
+  {
+    "revision": "901bd3d20adef3b0ff293d4d45710e3c",
+    "url": "/coin-app/static/media/waveSvg.901bd3d2.svg"
+  },
+  {
+    "revision": "9dc0d815d02220344a1c363f2c527959",
+    "url": "/coin-app/static/media/welcomeSvg.9dc0d815.svg"
+  },
+  {
+    "revision": "92915604b595fae1e9bc73153554956c",
+    "url": "/coin-app/static/media/worldSvg.92915604.svg"
+  }
+]);
