a013f889c1d0b5e2927ac766ce7636077ccf19cd
cipher-tech <nickchibuikem@gmail.com>
2020-08-01 01:51:01 +0100

    react lazy and suspanse implemented

--- src/App.js
+++ src/App.js
@@ -1,43 +1,46 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { ThemeProvider } from 'styled-components';
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 import { BrowserRouter as Router, Switch, Route, withRouter, } from "react-router-dom"
 //HashRouter BrowserRouter 
-
-import reduxStore from "./reduxStore"
-import { GlobalStyle, Container } from './components/styledComponents';
-import Main from './pages/main';
-import routes from './navigation/routes';
-import Dashboard from './pages/admin/dashboard';
-import DashboardLayout from './pages/admin/dashboardLayout';
-import Rates from "./pages/admin/rates/rates"
-import AdminSellBitcoin from './pages/admin/sellBitcion/sellBitcion';
-import AdminSellGiftCard from './pages/admin/sellGiftCard/sellGiftCard';
-import AdminTransaction from './pages/admin/transaction/transaction';
-import Login from './pages/auth/login/login';
-import SignUp from './pages/auth/signup/signup';
 import { ContactUs } from './pages/contact/contactUs';
-import Faq from './pages/faq/faq';
 import { Foot, Navbar } from './components';
+import reduxStore from "./reduxStore"
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import MasterDashboardLayout from './pages/masterAdmin/masterDashboardLayout';
-import MasterAdminRates from "./pages/masterAdmin/rates/rates";
-import AdminUsers from './pages/masterAdmin/users/users';
-import UserVerify from './pages/admin/verify/verify';
-import VerifyUsers from './pages/masterAdmin/verify/verifyUsers';
-import DepositRequest from './pages/admin/depositRequest/depositRequest';
-import WidthdrawlRequest from './pages/admin/widthdrawlRequest/widthdrawlRequest';
-import AdminDepositRequest from './pages/masterAdmin/depositRequest/depositRequest';
-import AdminWidthdrawlRequest from './pages/masterAdmin/widthdrawlRequest/widthdrawlRequest';
-import AboutUS from './pages/aboutUs/aboutUs';
-import Policies from './pages/policies/policies';
-import UpdateUserInfo from './pages/admin/updateUserInfo/updateUserInfo';
-import ContextProvider from './context/contextProvider';
-import Orders from './pages/masterAdmin/orders/orders';
-import UserBuySell from './pages/userBuySell/userBuySell';
-import UserRates from './pages/userRates/userRates';
+import { GlobalStyle, Container } from './components/styledComponents';
+
+import routes from './navigation/routes';
+const Main = lazy(() => import('./pages/main'));
+const AboutUS = lazy(() => import('./pages/aboutUs/aboutUs'));
+const UserRates = lazy(() => import('./pages/userRates/userRates'));
+const Dashboard = lazy(() => import('./pages/admin/dashboard'));
+const DashboardLayout = lazy(() => import('./pages/admin/dashboardLayout'));
+const Rates = lazy(() => import("./pages/admin/rates/rates"));
+const AdminSellBitcoin = lazy(() => import('./pages/admin/sellBitcion/sellBitcion'));
+const AdminSellGiftCard = lazy(() => import('./pages/admin/sellGiftCard/sellGiftCard'));
+const AdminTransaction = lazy(() => import('./pages/admin/transaction/transaction'));
+const Login = lazy(() => import('./pages/auth/login/login'));
+const SignUp = lazy(() => import('./pages/auth/signup/signup'));
+const Faq = lazy(() => import('./pages/faq/faq'));
+const MasterDashboardLayout = lazy(() => import('./pages/masterAdmin/masterDashboardLayout'));
+const MasterAdminRates = lazy(() => import("./pages/masterAdmin/rates/rates"));
+const AdminUsers = lazy(() => import('./pages/masterAdmin/users/users'));
+const UserVerify = lazy(() => import('./pages/admin/verify/verify'));
+const VerifyUsers = lazy(() => import('./pages/masterAdmin/verify/verifyUsers'));
+const DepositRequest = lazy(() => import('./pages/admin/depositRequest/depositRequest'));
+const WidthdrawlRequest = lazy(() => import('./pages/admin/widthdrawlRequest/widthdrawlRequest'));
+const AdminDepositRequest = lazy(() => import('./pages/masterAdmin/depositRequest/depositRequest'));
+const AdminWidthdrawlRequest = lazy(() => import('./pages/masterAdmin/widthdrawlRequest/widthdrawlRequest'));
+const Policies = lazy(() => import('./pages/policies/policies'));
+const UpdateUserInfo = lazy(() => import('./pages/admin/updateUserInfo/updateUserInfo'));
+const ContextProvider = lazy(() => import('./context/contextProvider'));
+const Orders = lazy(() => import('./pages/masterAdmin/orders/orders'));
+const UserBuySell = lazy(() => import('./pages/userBuySell/userBuySell'));
+
+
+
 
 const theme = {
 	colorPrimary: "#581b98",
@@ -88,60 +91,62 @@ function App() {
 	return (
 		<Provider store={createStore(reduxStore)}>
 			<Router basename={process.env.PUBLIC_URL}>
-				<Switch>
-					<ThemeProvider theme={theme}>
-						<GlobalStyle />
-						<ContextProvider>
-							<Container>
-								<Route path={["/", "/contact", "/faq", "/about", "/policies", "/buySell", "/rates"]} exact>
-									<Navbar />
-									<Route exact path={routes.public.home} component={Main} />
-									<Route exact path={routes.public.contact} component={ContactUs} />
-									<Route exact path={routes.public.faq} component={Faq} />
-									<Route exact path={routes.public.about} component={AboutUS} />
-									<Route exact path={routes.public.policies} component={Policies} />
-									<Route exact path={routes.public.buy_sell} component={UserBuySell} />
-									<Route exact path={routes.public.rates} component={UserRates} />
-									<Foot />
-								</Route>
-								<Route exact path={routes.public.login} component={withRouter(Login)} />
-								<Route exact path={routes.public.signUp} component={withRouter(SignUp)} />
-								<Route path="/admin/:path?" exact>
-									<DashboardLayout>
-										<Switch>
-											<Route exact path={routes.admin.index} component={Dashboard} />
-											<Route exact path={routes.admin.rates} component={Rates} />
-											<Route exact path={routes.admin.sellBitcoin} component={AdminSellBitcoin} />
-											<Route exact path={routes.admin.sellGiftcard} component={AdminSellGiftCard} />
-											<Route exact path={routes.admin.transcation} component={AdminTransaction} />
-											<Route exact path={routes.admin.verify} component={withRouter(UserVerify)} />
-											<Route exact path={routes.admin.deposit} component={DepositRequest} />
-											<Route exact path={routes.admin.widthdrawl} component={WidthdrawlRequest} />
-											<Route exact path={routes.admin.updateInfo} component={UpdateUserInfo} />
-										</Switch>
-									</DashboardLayout>
-								</Route>
-								<Route path={"/master-admin/:path?"} exact>
-									<MasterDashboardLayout>
-										<Switch>
-											<Route exact path={routes.masterAdmin.index} component={Dashboard} />
-											<Route exact path={routes.masterAdmin.users} component={AdminUsers} />
-											<Route exact path={routes.masterAdmin.rates} component={MasterAdminRates} />
-											<Route exact path={routes.masterAdmin.sellBitcoin} component={AdminSellBitcoin} />
-											<Route exact path={routes.masterAdmin.sellGiftcard} component={AdminSellGiftCard} />
-											<Route exact path={routes.masterAdmin.transcation} component={AdminTransaction} />
-											<Route exact path={routes.masterAdmin.orders} component={Orders} />
-											<Route exact path={routes.masterAdmin.verify} component={VerifyUsers} />
-											<Route exact path={routes.masterAdmin.deposit} component={AdminDepositRequest} />
-											<Route exact path={routes.masterAdmin.widthdrawl} component={AdminWidthdrawlRequest} />
-										</Switch>
-									</MasterDashboardLayout>
-								</Route>
-							</Container>
-						</ContextProvider>
+				<Suspense fallback={<div>Loading...</div>}>
+					<Switch>
+						<ThemeProvider theme={theme}>
+							<GlobalStyle />
+							<ContextProvider>
+								<Container>
+									<Route path={["/", "/contact", "/faq", "/about", "/policies", "/buySell", "/rates"]} exact>
+										<Navbar />
+										<Route exact path={routes.public.home} component={Main} />
+										<Route exact path={routes.public.contact} component={ContactUs} />
+										<Route exact path={routes.public.faq} component={Faq} />
+										<Route exact path={routes.public.about} component={AboutUS} />
+										<Route exact path={routes.public.policies} component={Policies} />
+										<Route exact path={routes.public.buy_sell} component={UserBuySell} />
+										<Route exact path={routes.public.rates} component={UserRates} />
+										<Foot />
+									</Route>
+									<Route exact path={routes.public.login} component={withRouter(Login)} />
+									<Route exact path={routes.public.signUp} component={withRouter(SignUp)} />
+									<Route path="/admin/:path?" exact>
+										<DashboardLayout>
+											<Switch>
+												<Route exact path={routes.admin.index} component={Dashboard} />
+												<Route exact path={routes.admin.rates} component={Rates} />
+												<Route exact path={routes.admin.sellBitcoin} component={AdminSellBitcoin} />
+												<Route exact path={routes.admin.sellGiftcard} component={AdminSellGiftCard} />
+												<Route exact path={routes.admin.transcation} component={AdminTransaction} />
+												<Route exact path={routes.admin.verify} component={withRouter(UserVerify)} />
+												<Route exact path={routes.admin.deposit} component={DepositRequest} />
+												<Route exact path={routes.admin.widthdrawl} component={WidthdrawlRequest} />
+												<Route exact path={routes.admin.updateInfo} component={UpdateUserInfo} />
+											</Switch>
+										</DashboardLayout>
+									</Route>
+									<Route path={"/master-admin/:path?"} exact>
+										<MasterDashboardLayout>
+											<Switch>
+												<Route exact path={routes.masterAdmin.index} component={Dashboard} />
+												<Route exact path={routes.masterAdmin.users} component={AdminUsers} />
+												<Route exact path={routes.masterAdmin.rates} component={MasterAdminRates} />
+												<Route exact path={routes.masterAdmin.sellBitcoin} component={AdminSellBitcoin} />
+												<Route exact path={routes.masterAdmin.sellGiftcard} component={AdminSellGiftCard} />
+												<Route exact path={routes.masterAdmin.transcation} component={AdminTransaction} />
+												<Route exact path={routes.masterAdmin.orders} component={Orders} />
+												<Route exact path={routes.masterAdmin.verify} component={VerifyUsers} />
+												<Route exact path={routes.masterAdmin.deposit} component={AdminDepositRequest} />
+												<Route exact path={routes.masterAdmin.widthdrawl} component={AdminWidthdrawlRequest} />
+											</Switch>
+										</MasterDashboardLayout>
+									</Route>
+								</Container>
+							</ContextProvider>
 
-					</ThemeProvider>
-				</Switch>
+						</ThemeProvider>
+					</Switch>
+				</Suspense>
 			</Router>
 		</Provider>
 	);
